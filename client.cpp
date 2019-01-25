b7bfa3a79f3b3d92c0168b71c08e2a672029d1f3
rlin2k1 <rlin2k1@gmail.com>
2019-01-25 13:19:24 -0800

    Basic Client and Server File Transfer Working

--- client.cpp
+++ client.cpp
@@ -13,117 +13,102 @@ Date Created:
 // Import Statements for the Necessary Packages
 // -------------------------------------------------------------------------- //
 #include <sys/types.h>
-#include <sys/socket.h>
+#include <sys/socket.h> //For the Socket()
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <string.h>
-#include <stdio.h>
-#include <errno.h>
+#include <string.h> //String Library
+#include <stdio.h> //Standard Input/Ouput Library
+#include <errno.h> //Error Handling
 #include <unistd.h>
-#include <thread>
-#include <iostream>
-#include <sstream>
+#include <thread> //For Multi-Threading - Handling Multiple Clients
+#include <iostream> //Input and Output
 
-#include <getopt.h>
+using namespace std; //Using the Standard Namespace
 
-using namespace std;
-int
-main(int argc, char *argv[])
+#define BUFLENGTH 1024
+
+int main(int argc, char *argv[]) //Main Function w/ Arguments from Command Line
 {
   // ------------------------------------------------------------------------ //
   // Error Handling from Arguments
   // ------------------------------------------------------------------------ //
   if(argc != 4) {
-    cerr << "Need 3 Arguments: HostName, Port Number, and File Name to Transfer" << endl;
+    cerr << "ERROR: Need 3 Arguments: HostName, Port Number, and File Name to " 
+      << "Transfer" << endl;
     exit(1);
   }
-  const char* hostname = argv[1];
-  int port_number = stoi(argv[2]);
-  if(port_number <= 1023){
-    cerr << "Reserved Port Number Detected. Please Specify a Port Number Greater than 1023" << endl;
-    exit(1);
-  }
-  string file_name = argv[3]; //Assume File Name is Always Correct
 
-  cout << "Hostname to Be Connected to: " << hostname << endl;
-  cout << "Port Number to Be Connected to: " << port_number << endl;
-  cout << "File Name to Transfer: " << file_name << endl;
+  const char* hostname = argv[1]; //The HostName/IP Address is 1st Argument
+  int port_number = stoi(argv[2]); //The Port Number is the 2nd Argument
+  if(port_number <= 1023){
+    cerr << "ERROR: Reserved Port Number Detected. Please Specify a Port Number"
+      << " Greater than 1023" << endl;
+    exit(1);
+  }
+  string file_name = argv[3]; //Assume File Name is Always Correct - 3rd Arg
+
+  cerr << "Hostname to Be Connected to: " << hostname << endl;
+  cerr << "Port Number to Be Connected to: " << port_number << endl;
+  cerr << "File Name to Transfer: " << file_name << endl;
 
   // ------------------------------------------------------------------------ //
   // Create a Socket using TCP IP
   // ------------------------------------------------------------------------ //
+  //sockfd contains the file descriptor to access the Socket
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-  // struct sockaddr_in addr;
-  // addr.sin_family = AF_INET;
-  // addr.sin_port = htons(40001);     // short, network byte order
-  // addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  // memset(addr.sin_zero, '\0', sizeof(addr.sin_zero));
-  // if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
-  //   perror("bind");
-  //   return 1;
-  // }
-  struct sockaddr_in serverAddr;
-  serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(port_number);         // Short, Network Byte Order
-  serverAddr.sin_addr.s_addr = inet_addr(hostname);
+  struct sockaddr_in serverAddr; //Get Ready to Store the Server Address
+  serverAddr.sin_family = AF_INET; //Type
+  serverAddr.sin_port = htons(port_number); //Define the Port Number
+  //Short, Network Byte Order
+  serverAddr.sin_addr.s_addr = inet_addr(hostname); //Where is the Server IP
   memset(serverAddr.sin_zero, '\0', sizeof(serverAddr.sin_zero));
+  //Memset to Null Bytes
 
   // ------------------------------------------------------------------------ //
   // Connect to the Server
   // ------------------------------------------------------------------------ //
-  if (connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
-    perror("connect");
-    return 2;
+  if (connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1)
+  //Attempt to Connect to the Given HostName/IP Address and Port Number
+  {
+    cerr << "ERROR: Connect Function Failed" << endl;
+    exit(2);
   }
 
-  struct sockaddr_in clientAddr;
-  socklen_t clientAddrLen = sizeof(clientAddr);
-  if (getsockname(sockfd, (struct sockaddr *)&clientAddr, &clientAddrLen) == -1) {
-    perror("getsockname");
-    return 3;
+  struct sockaddr_in clientAddr; //Save the Client Address
+  socklen_t clientAddrLen = sizeof(clientAddr); //Size of the Client Address
+  if (getsockname(sockfd, (struct sockaddr *)&clientAddr, &clientAddrLen) == -1)
+  {
+    //Make Sure Client Address is OK
+    cerr << "ERROR: Getsockname Function Failed" << endl;
+    exit(3);
   }
 
-  char ipstr[INET_ADDRSTRLEN] = {'\0'};
+  char ipstr[INET_ADDRSTRLEN] = {'\0'}; //Set String to NullByte
   inet_ntop(clientAddr.sin_family, &clientAddr.sin_addr, ipstr, sizeof(ipstr));
-  std::cout << "Set up a connection from: " << ipstr << ":" <<
-    ntohs(clientAddr.sin_port) << std::endl;
+  //Put Client Information in 'ipstr'
+  cerr << "Set Up a Connection From: " << ipstr << ":" << \
+  ntohs(clientAddr.sin_port) << endl; //State Who the Client Is
 
-
-  // -------------------------------------------------------------------------- //
+  // ------------------------------------------------------------------------ //
   // Send/Receive Data to/from Connection
-  // -------------------------------------------------------------------------- //
-  bool isEnd = false;
-  std::string input;
-  char buf[20] = {0};
-  std::stringstream ss;
+  // ------------------------------------------------------------------------ //
+  FILE *fs = fopen(file_name.c_str(), "r"); //Open the File for Reading
+  char buf[BUFLENGTH] = {0}; //Set the Buffer as BUFLENGTH Characters
 
-  while (!isEnd) {
-    memset(buf, '\0', sizeof(buf));
+  memset(buf, '\0', sizeof(buf)); //Reset Buffer Every Time
 
-    std::cout << "send: ";
-    std::cin >> input;
-    if (send(sockfd, input.c_str(), input.size(), 0) == -1) {
-      perror("send");
-      return 4;
+  int fs_block_sz;
+  while((fs_block_sz = fread(buf, sizeof(char), 500, fs)) > 0)
+  {
+    cerr << "Send: " << buf << endl; //Output the Buffer into Standard Output;
+    if (send(sockfd, buf, fs_block_sz, 0) == -1) {
+      //Attempt to Send the BUFFER String Over the Socket Connection
+      cerr << "ERROR: Send Function Failed" << endl;
+      exit(4);
     }
-
-
-    if (recv(sockfd, buf, 20, 0) == -1) {
-      perror("recv");
-      return 5;
-    }
-    ss << buf << std::endl;
-    std::cout << "echo: ";
-    std::cout << buf << std::endl;
-
-    if (ss.str() == "close\n")
-      break;
-
-    ss.str("");
   }
+  close(sockfd); //Finally Close the Connection
 
-  close(sockfd);
-
-  return 0;
+  return 0; //Exit Normally
 }
--- server.cpp
+++ server.cpp
@@ -2,6 +2,8 @@
 The server listens for TCP connections and saves all the received data from
 the client in a file.
 
+Notes: Mainly use select() for 15 Second Timer!
+
 Author(s):
   Roy Lin
 
@@ -13,123 +15,261 @@ Date Created:
 // Import Statements for the Necessary Packages
 // -------------------------------------------------------------------------- //
 #include <sys/types.h>
-#include <sys/socket.h>
+#include <sys/socket.h> //For the Socket()
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <string.h>
-#include <stdio.h>
-#include <errno.h>
+#include <string.h> //String Library
+#include <stdio.h> //Standard Input/Ouput Library
+#include <errno.h> //Error Handling
 #include <unistd.h>
-#include <thread>
-#include <iostream>
-#include <sstream>
+#include <thread> //For Multi-Threading - Handling Multiple Clients
+#include <iostream> //Input and Output
+#include <sstream> //String Stream
 
-using namespace std;
+#include <signal.h> //For Signal Processing and KILL() Command
+#include <fcntl.h> //For Socket Non Blocking
+#include <chrono> //For Timing
 
-int
-main(int argc, char *argv[])
+#include <stdlib.h> //For Standard Library
+#include <sys/wait.h>
+
+#include <fstream>
+#include <sys/select.h> //For the Select()
+
+using namespace std; //Using the Standard Namespace
+
+#define BUFLENGTH 1024
+
+struct arg_struct { 
+    int socket;          // Socket File Descriptor
+    int num;             // File Number - Order Connected to the Socket
+};
+
+string file_directory = "";
+
+//No Mutex Because No Race Conditions When Reading from Socket or Writing File
+
+void *socketThread(void *arg)
 {
+  int newSocket = ((struct arg_struct *)arg)->socket;
+  int num = ((struct arg_struct *)arg)->num;
+
+  fd_set active_fd_set;
+  fd_set working_fd_set;
+  FD_ZERO (&active_fd_set); //Zero Out the Active File Descriptor Set
+  FD_SET (newSocket, &active_fd_set); //Add the newSocket to the Working Set
+
+  string file_path = file_directory + "/" + to_string(num) + ".file"; //FileName
+  FILE *fs = fopen(file_path.c_str(), "wb"); //Open the File for Modification
+
+  char error[25] = "ERROR: Select Timed Out!";
+  char buf[BUFLENGTH] = {0}; //Set the Buffer as BUFLENGTH Characters
+
+  while(true)
+  { //When We are Not Over
+    memset(buf, '\0', sizeof(buf)); //Reset Buffer Every Time
+
+    working_fd_set = active_fd_set; //Determine the Working Set
+    struct timeval tv = {15, 0};   //Set the Timeout Interval as 15 Seconds!
+
+    int rc = select(newSocket + 1, &working_fd_set, NULL, NULL, &tv);
+    if (rc < 0)
+    {
+        cerr << "ERROR: SELECT() FAILED" << endl;
+        close(newSocket); //Finally Close the Connection
+        pthread_exit(NULL);
+        exit(7);
+    }
+    if (rc == 0)
+    {
+        FILE *fd = freopen(file_path.c_str(), "w", fs); //Rewrite the File!
+        fwrite(error, 1, 10, fd); //Write the Error Message into the File
+        cerr << "ERROR: Select Timed Out!" << endl;
+        close(newSocket); //Finally Close the Connection
+        pthread_exit(NULL);
+        exit(7);
+    }
+
+    int fr_block_sz = 0; //We Got Something!
+    while((fr_block_sz = recv(newSocket, buf, BUFLENGTH, 0)) > 0)
+    {
+        cerr << buf << endl; //Output the Buffer into Standard Output
+        int write_sz = fwrite(buf, sizeof(char), fr_block_sz, fs); //Write File
+        fflush(fs); //Make Sure Everything is Written to File!
+        if(write_sz < fr_block_sz)
+        {
+          cerr << "ERROR: Write Operation Failed on the Server" << endl;
+        }
+        memset(buf, '\0', sizeof(buf)); //Reset Buffer Every Time
+        if (fr_block_sz == 0 || fr_block_sz != BUFLENGTH) 
+        {
+          break; //Done Reading!
+        }
+    }
+    if(fr_block_sz < 0)
+    {
+        if (errno == EAGAIN)
+        {
+          cerr << "ERROR: RECV() TIMED OUT" << endl;
+        }
+        else
+        {
+          cerr << "ERROR: RECV() FAILED" << endl;
+          close(newSocket); //Finally Close the Connection
+          pthread_exit(NULL);
+          exit(7);
+        }
+    }
+  }
+  close(newSocket); //Finally Close the Connection
+  pthread_exit(NULL);
+}
+
+void sigquit_handler(int signum) {
+  cerr << "ERROR: Received SIGQUIT Signal" << endl;
+  exit(0);
+}
+
+void sigterm_handler(int signum) {
+  cerr << "ERROR: Received SIGTERM Signal" << endl;
+  exit(0);
+}
+
+int main(int argc, char *argv[]) //Main Function w/ Arguments from Command Line
+{
+  // -------------------------------------------------------------------------- //
+  // Signal Handling
+  // -------------------------------------------------------------------------- //
+  signal(SIGQUIT, sigquit_handler);
+  signal(SIGTERM, sigterm_handler);
+
   // ------------------------------------------------------------------------ //
   // Error Handling from Arguments
   // ------------------------------------------------------------------------ //
   if(argc != 3) {
-    cerr << "Need 2 Arguments: Port Number and File Directory" << endl;
+    cerr << "ERROR: Need 2 Arguments: Port Number and File Directory" << endl;
     exit(1);
   }
 
-  int port_number = stoi(argv[1]);
+  int port_number = stoi(argv[1]); //The Port Number is the First Argument
   if(port_number <= 1023){
-    cerr << "Reserved Port Number Detected. Please Specify a Port Number Greater than 1023" << endl;
-    exit(1);
+    cerr << "ERROR: Reserved Port Number Detected. Please Specify a Port Number"
+      << " Greater than 1023" << endl;
+    exit(2);
   }
-  string file_directory = argv[2]; //Assume Directory is Always Correct
+  file_directory = argv[2]; //Assume Directory is Always Correct-2nd Arg
 
-  cout << "Port Number to Be Connected to: " << port_number << endl;
-  cout << "Directory to Save Transferred File in: " << file_directory << endl;
+  cerr << "Port Number to Be Connected to: " << port_number << endl;
+  cerr << "Directory to Save Transferred File in: " << file_directory << endl;
   
   // ------------------------------------------------------------------------ //
   // Create a Socket using TCP IP
   // ------------------------------------------------------------------------ //
+  //sockfd contains the file descriptor to access the Socket
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
   // ------------------------------------------------------------------------ //
-  // Allow Others to Reuse the Address
+  // Allow Others to Reuse the Address - Error Handling
   // ------------------------------------------------------------------------ //
   int yes = 1;
   if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-    perror("setsockopt");
+    cerr << "ERROR: Set Socket Options Failed" << endl;
     return 1;
   }
+  // fcntl(sockfd, F_SETFL, O_NONBLOCK);
+  // if (fcntl(sockfd, F_SETFL, O_NONBLOCK)  == -1) {
+  //   perror("fcntyl");
+  //   return 1;
+  // }
 
   // ------------------------------------------------------------------------ //
   // Bind Address to Socket
   // ------------------------------------------------------------------------ //
   struct sockaddr_in addr;
-  addr.sin_family = AF_INET;
-  addr.sin_port = htons(port_number);               // Short, Network Byte Order
-  addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  memset(addr.sin_zero, '\0', sizeof(addr.sin_zero));
+  addr.sin_family = AF_INET; //Type
+  addr.sin_port = htons(port_number); //DefinePortNumber - ShortNetworkByteOrder
+  addr.sin_addr.s_addr = inet_addr("127.0.0.1"); //Where Hosting From
+  memset(addr.sin_zero, '\0', sizeof(addr.sin_zero)); //Memset to Null Bytes
 
   if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
-    perror("bind");
-    return 2;
+    //Bind the Port and Hostname/IP Address Together
+    cerr << "ERROR: Error Binding the Port and the HostName/IP Address Together"
+      << endl;
+    exit(2);
   }
 
   // ------------------------------------------------------------------------ //
   // Set Socket to Listen Status
   // ------------------------------------------------------------------------ //
-  if (listen(sockfd, 1) == -1) {
-    perror("listen");
-    return 3;
+  if (listen(sockfd, 100) == -1) {
+    //Start Listening to the Connections
+    cerr << "ERROR: Listen() Failed" << endl;
+    exit(2);
   }
 
   // ------------------------------------------------------------------------ //
-  // Accept a New Connection
+  // Select Function Setup - Do We Really Need This? Since We Are Only Using 1SD
   // ------------------------------------------------------------------------ //
-  struct sockaddr_in clientAddr;
-  socklen_t clientAddrSize = sizeof(clientAddr);
-  int clientSockfd = accept(sockfd, (struct sockaddr*)&clientAddr, &clientAddrSize);
+  fd_set active_fd_set;
+  fd_set working_fd_set;
 
-  if (clientSockfd == -1) {
-    perror("accept");
-    return 4;
+  FD_ZERO (&active_fd_set); //Zero Out the Active File Descriptor Set
+  FD_SET (sockfd, &active_fd_set); //Add the newSocket to the Working Set
+
+  //----------------------------------------------------------------------------
+  int newSocket;
+  pthread_t tid[100];
+  int i = 1;
+  while(1)
+  {
+      struct sockaddr_in clientAddr; //Place to Store the Client Address
+      socklen_t clientAddrSize = sizeof(clientAddr); //Store Client Address Size
+      //Accept Call Creates a New Socket for the Incoming Connection
+
+      working_fd_set = active_fd_set; //Determine the Working Set
+
+      int rc = select(sockfd + 1, &working_fd_set, NULL, NULL, NULL);
+      if (rc < 0)
+      {
+          cerr << "ERROR: SELECT() FAILED" << endl;
+          close(sockfd); //Finally Close the Connection
+          exit(7);
+      }
+
+      newSocket = accept(sockfd, (struct sockaddr *) &clientAddr,
+       &clientAddrSize);
+      //For each Client Request Creates a Thread and Assign the Client Request 
+      //to it to Process so the Main Thread can Entertain Next Request
+      if (newSocket == -1) { //Error Handling for Bad Connection
+        cerr << "ERROR: ACCEPT() Failed" << endl;
+        exit(2);
+      }
+
+      char ipstr[INET_ADDRSTRLEN] = {'\0'}; //Set String to NullByte
+      inet_ntop(clientAddr.sin_family, &clientAddr.sin_addr, ipstr,
+       sizeof(ipstr));
+      //Put Client Information in 'ipstr'
+      cerr << "Accept a Connection From: " << ipstr << ":" << \
+      ntohs(clientAddr.sin_port) << endl; //State Who Was Accepted
+
+      struct arg_struct arguments;
+      arguments.socket = newSocket;
+      arguments.num = i;
+
+      if(pthread_create(&tid[i], NULL, socketThread, (void*) &arguments) != 0 ){
+        cerr << "Failed to Create Thread!" << endl;
+      }
+      i = i + 1;
+      if( i >= 100)
+      {
+        i = 0;
+        while(i < 100)
+        {
+          pthread_join(tid[i++],NULL);
+        }
+        i = 0;
+      }
   }
-
-  char ipstr[INET_ADDRSTRLEN] = {'\0'};
-  inet_ntop(clientAddr.sin_family, &clientAddr.sin_addr, ipstr, sizeof(ipstr));
-  std::cout << "Accept a connection from: " << ipstr << ":" <<
-    ntohs(clientAddr.sin_port) << std::endl;
-
-  // ------------------------------------------------------------------------ //
-  // Read/Write Data From/Into the Connection
-  // ------------------------------------------------------------------------ //
-  bool isEnd = false;
-  char buf[20] = {0};
-  std::stringstream ss;
-
-  while (!isEnd) {
-    memset(buf, '\0', sizeof(buf));
-
-    if (recv(clientSockfd, buf, 20, 0) == -1) {
-      perror("recv");
-      return 5;
-    }
-
-    ss << buf << std::endl;
-    std::cout << buf << std::endl;
-
-    if (send(clientSockfd, buf, 20, 0) == -1) {
-      perror("send");
-      return 6;
-    }
-
-    if (ss.str() == "close\n")
-      break;
-
-    ss.str("");
-  }
-
-  close(clientSockfd);
-
+  close(sockfd);
   return 0;
 }
